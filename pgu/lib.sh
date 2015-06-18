f380d474a558f005cc0a6a8742596bcd879aa793
Dmitry Simakov <basilio@j-vista.ru>
2015-06-18 22:38:13 +0300

    skip https cert check, alternative urlencoding with curl

--- pgu/lib.sh
+++ pgu/lib.sh
@@ -9,7 +9,7 @@ init() {
     resp=`mktemp /tmp/curl-pgu-json.XXXXX`
     cjar=`mktemp /tmp/curl-pgu-cookies.XXXXX`
     # get cookies
-    curl -c $cjar -s https://login.mos.ru/eaidit/eaiditweb/openouterlogin.do > /dev/null
+    curl -c $cjar -k -s https://login.mos.ru/eaidit/eaiditweb/openouterlogin.do > /dev/null
 }
 cleanup() {
     # remove temp files
@@ -18,12 +18,10 @@ cleanup() {
 }
 
 loginPgu() {
-    # replace @ with %40 in login
-    login=`echo "$login"|sed -e 's/@/%40/'`
     # post login data and check redirect URL - doesnt' work
 #    if ! curl -c $cjar -b $cjar -s -D - -o /dev/null -d "username=$login&password=$password" https://login.mos.ru/eaidit/eaiditweb/outerlogin.do | grep -qF "https://login.mos.ru/eaidit/eaiditweb/loginok.do"; then
     # post login data, follow redirects, check resulting page
-    if ! curl -c $cjar -b $cjar -s -L -d "username=$login&password=$password" https://login.mos.ru/eaidit/eaiditweb/outerlogin.do | grep -q "Your login was successful"; then
+    if ! curl -c $cjar -b $cjar -k -s -L --data-urlencode "username=$login" --data-urlencode "password=$password" https://login.mos.ru/eaidit/eaiditweb/outerlogin.do | grep -q "Your login was successful"; then
         echo "Login failed!" >&2
         cleanup
         exit 1
@@ -36,24 +34,24 @@ getWaterCounterIds() {
 
 getWaterIndications() {
     # get water counters
-    curl -c $cjar -b $cjar -s -d "getCountersInfo=true&requestParams%5Bpaycode%5D=$paycode" https://pgu.mos.ru/ru/application/guis/1111/
+    curl -c $cjar -b $cjar -k -s -d "getCountersInfo=true&requestParams%5Bpaycode%5D=$paycode" https://pgu.mos.ru/ru/application/guis/1111/
 }
 
 removeWaterIndication() {
-    curl -c $cjar -b $cjar -s -d "removeCounterIndication=true&values%5Bpaycode%5D=$paycode&values%5BcounterId%5D=$1" https://pgu.mos.ru/ru/application/guis/1111/ > /dev/null
+    curl -c $cjar -b $cjar -k -s -d "removeCounterIndication=true&values%5Bpaycode%5D=$paycode&values%5BcounterId%5D=$1" https://pgu.mos.ru/ru/application/guis/1111/ > /dev/null
 }
 
 setWaterIndications() {
     hot="$1"
     cold="$2"
     [ "$hot" -gt "$cold" ] && echo "Error: Hot counter value ($hot) > cold counter value ($cold)!" && exit 1
-    curl -c $cjar -b $cjar -s -d "addCounterInfo=true&values%5Bpaycode%5D=$paycode&values%5Bindications%5D%5B0%5D%5BcounterNum%5D=$type_2&values%5Bindications%5D%5B0%5D%5BcounterVal%5D=$hot&values%5Bindications%5D%5B0%5D%5Bperiod%5D=$dt&values%5Bindications%5D%5B0%5D%5Bnum%5D=&values%5Bindications%5D%5B1%5D%5BcounterNum%5D=$type_1&values%5Bindications%5D%5B1%5D%5BcounterVal%5D=$cold&values%5Bindications%5D%5B1%5D%5Bperiod%5D=$dt&values%5Bindications%5D%5B1%5D%5Bnum%5D=" https://pgu.mos.ru/ru/application/guis/1111/  > /dev/null
+    curl -c $cjar -b $cjar -k -s -d "addCounterInfo=true&values%5Bpaycode%5D=$paycode&values%5Bindications%5D%5B0%5D%5BcounterNum%5D=$type_2&values%5Bindications%5D%5B0%5D%5BcounterVal%5D=$hot&values%5Bindications%5D%5B0%5D%5Bperiod%5D=$dt&values%5Bindications%5D%5B0%5D%5Bnum%5D=&values%5Bindications%5D%5B1%5D%5BcounterNum%5D=$type_1&values%5Bindications%5D%5B1%5D%5BcounterVal%5D=$cold&values%5Bindications%5D%5B1%5D%5Bperiod%5D=$dt&values%5Bindications%5D%5B1%5D%5Bnum%5D=" https://pgu.mos.ru/ru/application/guis/1111/  > /dev/null
 }
 
 printMosenergoLastValue() {
     echo "Previously sent values:"
     echo "Date		T1	T2	T3"
-    curl -c $cjar -b $cjar -s -d "getAction=auth&ls=$mosenergo_accnum&pu=$mosenergo_cntnum" https://pgu.mos.ru/ru/application/mosenergo/counters/ \
+    curl -c $cjar -b $cjar -k -s -d "getAction=auth&ls=$mosenergo_accnum&pu=$mosenergo_cntnum" https://pgu.mos.ru/ru/application/mosenergo/counters/ \
         | jq -r ".fields | .count_submit_date, .count_t1, .count_t2, .count_t3"  | paste -sd '			\n' -
 }
 
