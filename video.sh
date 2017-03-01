d033913f663e1596bb1b343ef484c618ef9c7140
Dmitry Simakov <dsimakov@consultant.ru>
2017-03-01 12:07:53 +0300

    video fixes

--- video.sh
+++ video.sh
@@ -23,12 +23,12 @@ init
 while true; do
     loginPgu
     while true; do
-        curl  -c $cjar -b $cjar -k -s -A "$ua" -L "https://video.mos.ru/camera/ajaxGetVideoUrl?id=$cam_id" > $resp
-        grep -q "https://login.mos.ru" $resp && echo "Relogin required" && break
+        curl -o $resp -c $cjar -b $cjar -k -s -A "$ua" -e "https://video.mos.ru/;auto" -L "https://video.mos.ru/camera/ajaxGetVideoUrl?id=$cam_id"
+        grep -q "https://login.mos.ru" $resp && echo "Warning: re-login requested">&2 && break
         m3u=`cat $resp | jq -r '.live | .ios | .url[0]'`
-        [ -z "$m3u" ] && echo "Error: couldn't get video URL" && exit 1
+        [ -z "$m3u" ] && echo "Error: couldn't get playlist URL">&2 && exit 1
         ts_url=`curl -c $cjar -b $cjar -k -A "$ua" -s -L "$m3u" | grep -m 1 "http" | tr -d '\r'`
-        [ -z "$ts_url" ] && echo "Error: couldn't get m3u URL" && sleep 10 && continue
+        [ -z "$ts_url" ] && echo "Warning: failed get video stream URL">&2 && sleep 10 && continue
         [ ! -d "video/$cam_id" ] && mkdir -p "video/$cam_id"
         curl -c $cjar -b $cjar -k -A "$ua" -L "$ts_url" -o video/$cam_id/`date +%s`.ts
         done
